66142aa4381166bc960fc4ed219fade019648165
kcaliban <krausef@posteo.net>
2022-11-09 17:23:09 +0100

    Fix dependency injection for docking publisher

--- AsyncAPI/Publishers/DockingPublisher.cs
+++ AsyncAPI/Publishers/DockingPublisher.cs
@@ -3,13 +3,14 @@ using Models;
 
 namespace AsyncAPI.Publishers;
 
-public class DockingPublisher : IDockingPublisher
+public class DockingPublisher : IDockingPublisher, IDisposable
 {
     IPublishEndpoint _publishEndpoint;
 
     public DockingPublisher(IPublishEndpoint publishEndpoint)
     {
         _publishEndpoint = publishEndpoint;
+        Console.WriteLine("Created docking publisher");
     }
 
     public async Task PublishDocking(Docking docking)
@@ -17,4 +18,5 @@ public class DockingPublisher : IDockingPublisher
         await _publishEndpoint.Publish<Docking>(docking);
     }
 
+    public void Dispose() {}
 }
